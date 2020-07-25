0ded6125f61e3ae5313029fde1129fe9847eaae1
ShavJohn <shavarsh.garabedian@gmail.com>
2020-07-25 05:36:50 +0400

    workin vue

---
+++ project-frontend/src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-link :to="{name: 'HelloWorld'}">Main</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
